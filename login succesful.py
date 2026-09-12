9028a2cff15faa284aab1d168db4310afabd3625
simran89 <gujjarharsh628@gmail.com>
2026-09-12 11:29:06 +0530

    Add files via upload

---
+++ login succesful.py
@@ -0,0 +1,7 @@
+username=input("enter your username")
+password=input("enter your password")
+if username=="admin" and password=="1990":
+    print("login successful")
+else:
+    print("invalid username or password")
+

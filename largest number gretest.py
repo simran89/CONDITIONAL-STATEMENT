f2316b0d8ce484729b5b9d6a2bb9c87656192532
simran89 <gujjarharsh628@gmail.com>
2026-09-12 11:28:27 +0530

    Add files via upload

---
+++ largest number gretest.py
@@ -0,0 +1,9 @@
+a=int(input("enter your number"))
+b=int(input("enter your number"))
+c=int(input("enter your number"))
+if a>b and b>c:
+    print(a)
+elif b>a and b>c:
+    print(b)
+else:
+    print(c)

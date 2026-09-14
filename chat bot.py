e7f099d86560aa485f6df65208963b3e4692c489
simran89 <gujjarharsh628@gmail.com>
2026-09-14 12:03:18 +0530

    Add files via upload

---
+++ chat bot.py
@@ -0,0 +1,13 @@
+user=input("you")
+if user==("hii"):
+    print("hello")
+elif user==("How are you"):
+    print("I am fine")
+elif user==("How was your family"):
+    print("All is good")
+elif user==("whats going on"):
+    print("nothing")
+elif user==("Good by! Take care"):
+    print("Good bye! Take care")
+else:
+    print("Mujhe nhi pta")

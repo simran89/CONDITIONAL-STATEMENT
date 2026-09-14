b04aa3b5a88828710289273a60c507fc43c35c77
simran89 <gujjarharsh628@gmail.com>
2026-09-14 12:02:42 +0530

    Add files via upload

---
+++ python questions.py
@@ -0,0 +1,64 @@
+#Question 1
+score=0
+print("Question 1. What is python")
+print("a. Game")
+print("b. Software")
+print("c. Programing language")
+print("d. Hardware")
+answer= input("enter your answer")
+if answer=="c":
+    print("correct answer")
+    score+=1
+else:
+    print("incorrect answer")
+#Question 2
+print("Question 2. What is the correct file extension for python files?")
+print("a. .python")
+print("b. .py")
+print("c. .pt")
+print("d. .pyt")
+answer= input("enter your answer")
+if answer=="b":
+    print("correct answer")
+    score+=1
+else:
+    print("incorrect answer")
+#Question 3
+print("Question 3. Which function is used to display output of python?")
+print("a. input()")
+print("b. print()")
+print("c. output()")
+print("d. show()")
+answer= input("enter your answer")
+if answer=="b":
+    print("correct answer")
+    score+=1
+else:
+    print("incorrect answer")
+#Question 4
+print("Question 4. Which keyword is used to define a function in python?")
+print("a. function")
+print("b. def")
+print("c. fun")
+print("d. define")
+answer= input("enter your answer")
+if answer=="b":
+    print("correct answer")
+    score+=1
+else:
+    print("incorrect answer")
+#Question 5
+print("Question 5. Which symbol is used for comments in python?")
+print("a. //")
+print("b. #")
+print("c. /*")
+print("d. --")
+answer= input("enter your answer")
+if answer=="b":
+    print("correct answer")
+    score+=1
+else:
+    print("incorrect answer")
+print(score)
+
+
